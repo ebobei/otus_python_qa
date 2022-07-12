48b7206fb9e304f7d0d6132bf332a6dd12e9941d
Alexandr Geynts <ebobei55@gmail.com>
2022-07-12 23:59:17 +0600

    add tests

--- homework_2/tests/test_example.py
+++ homework_2/tests/test_example.py
@@ -1,6 +1,4 @@
 import pytest
-from math import pi
-from math import sqrt
 from homework_2.src.Circle import Circle
 from homework_2.src.Triangle import Triangle
 from homework_2.src.Rectangle import Rectangle
@@ -12,14 +10,20 @@ def test_circle_name():
     assert circle.name == "Circle"
 
 
-circle_area = [(0, 0),
-               (1, 0.7853981633974483),
-               (5, 19.634954084936208)
-               ]
+def test_circle_negative_name():
+    circle = Circle(10)
+    assert circle.name != "Carcle"
+
+
+circle_area = [
+    (0, 0),
+    (1, 0.7853981633974483),
+    (5, 19.634954084936208)
+]
 
 
 @pytest.mark.parametrize("diametr, area", circle_area)
-def test_circle_area(diametr, area):
+def test_circle_area_1(diametr, area):
     circle = Circle(diametr)
     assert circle.area == area
 
@@ -32,7 +36,7 @@ circle_perimetr = [
 
 
 @pytest.mark.parametrize("diametr, perimetr", circle_perimetr)
-def test_circle_area(diametr, perimetr):
+def test_circle_area_2(diametr, perimetr):
     circle = Circle(diametr)
     assert circle.perimetr == perimetr
 
@@ -43,9 +47,20 @@ def test_circle_add_area():
     assert circle.add_area(square) == 119.634954084936208
 
 
+def test_circle_add_negative_area():
+    circle = Circle(5)
+    square = Square(7)
+    assert circle.add_area(square) != 119.634954084936208
+
+
 def test_rectangle_name():
     rectangle = Rectangle(10, 5)
     assert rectangle.name == "Rectangle"
+
+
+def test_rectangle_negative_name():
+    rectangle = Rectangle(10, 5)
+    assert rectangle.name != "Ractangle"
 
 
 rectangle_area = [
@@ -55,7 +70,7 @@ rectangle_area = [
 
 
 @pytest.mark.parametrize("side_a, side_b, area", rectangle_area)
-def test_rectangle_area(side_a, side_b, area):
+def test_rectangle_area_1(side_a, side_b, area):
     rectangle = Rectangle(side_a, side_b)
     assert rectangle.area == area
 
@@ -67,7 +82,7 @@ rectangle_perimetr = [
 
 
 @pytest.mark.parametrize("side_a, side_b, perimetr", rectangle_perimetr)
-def test_rectangle_area(side_a, side_b, perimetr):
+def test_rectangle_area_2(side_a, side_b, perimetr):
     rectangle = Rectangle(side_a, side_b)
     assert rectangle.perimetr == perimetr
 
@@ -78,9 +93,20 @@ def test_rectangle_add_area():
     assert rectangle.add_area(square) == 75
 
 
+def test_rectangle_add_negative_area():
+    rectangle = Rectangle(20, 5)
+    square = Square(5)
+    assert rectangle.add_area(square) != 75
+
+
 def test_square_name():
     square = Square(10)
     assert square.name == "Square"
+
+
+def test_square_negative_name():
+    square = Square(10)
+    assert square.name != "Ssuare"
 
 
 square_area = [
@@ -91,7 +117,7 @@ square_area = [
 
 
 @pytest.mark.parametrize("side, area", square_area)
-def test_square_area(side, area):
+def test_square_area_1(side, area):
     square = Square(side)
     assert square.area == area
 
@@ -104,20 +130,31 @@ square_perimetr = [
 
 
 @pytest.mark.parametrize("side, perimetr", square_perimetr)
-def test_square_area(side, perimetr):
+def test_square_area_1(side, perimetr):
     square = Square(side)
     assert square.perimetr == perimetr
 
 
-def test_square_add_area():
+def test_square_add_area_2():
     square_a = Square(10)
     square_b = Square(5)
     assert square_a.add_area(square_b) == 125
 
 
+def test_square_add_negative_area_2():
+    square_a = Square(40)
+    square_b = Square(5)
+    assert square_a.add_area(square_b) != 125
+
+
 def test_triangle_name():
     triangle = Triangle(3, 4, 5)
     assert triangle.name == "Triangle"
+
+
+def test_triangle_negative_name():
+    triangle = Triangle(3, 4, 5)
+    assert triangle.name != "Tsiangle"
 
 
 triangle_area = [
@@ -127,7 +164,7 @@ triangle_area = [
 
 
 @pytest.mark.parametrize("side_a, side_b, side_c, area", triangle_area)
-def test_triangle_area(side_a, side_b, side_c, area):
+def test_triangle_area_1(side_a, side_b, side_c, area):
     triangle = Triangle(side_a, side_b, side_c)
     assert triangle.area == area
 
@@ -139,7 +176,7 @@ triangle_perimetr = [
 
 
 @pytest.mark.parametrize("side_a, side_b, side_c, perimetr", triangle_perimetr)
-def test_triangle_area(side_a, side_b, side_c, perimetr):
+def test_triangle_area_2(side_a, side_b, side_c, perimetr):
     triangle = Triangle(side_a, side_b, side_c)
     assert triangle.perimetr == perimetr
 
@@ -148,3 +185,9 @@ def test_triangle_add_area():
     triangle = Triangle(3, 4, 5)
     square = Square(5)
     assert triangle.add_area(square) == 31
+
+
+def test_triangle_add_negative_area():
+    triangle = Triangle(3, 5, 5)
+    square = Square(5)
+    assert triangle.add_area(square) != 31
